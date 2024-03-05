de97339902268034b39c5129c483406239c0cfaa
Kushwaha <karvika.kushwaha@dxc.com>
2024-03-05 20:46:37 +0530

    without strategy pattern

---
+++ src/WithoutStrategyPattern/OffRoadVehicle.java
@@ -0,0 +1,7 @@
+public class OffRoadVehicle extends Vehicle{
+    public void drive()
+    {
+        //code is duplicated in OffRoadVehicle and SportsVehicle
+        System.out.println("Sports drive capablity");
+    }
+}
---
+++ src/WithoutStrategyPattern/SportsVehicle.java
@@ -0,0 +1,7 @@
+public class SportsVehicle extends Vehicle{
+    public void drive()
+    {
+        //code is duplicated in OffRoadVehicle and SportsVehicle
+        System.out.println("Sports drive Capablity");
+    }
+}
